88d24dcf6038ab32da434c47fbf8cf93211bb126
SRIVIGHNATEJA <srivighnateja.vas55@gmail.com>
2026-08-10 14:20:15 +0530

    setup playwright test environment

---
+++ tests/playwright/playwright.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: '.',
+  fullyParallel: true,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://127.0.0.1:8080',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
